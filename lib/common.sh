bdc53b651c3f5f858e2563282f1961bbcc34d6f1
Andrei Dolgov <andrei@dolgov.dev>
2025-12-26 21:51:08 -0800

    Remove extra blank lines after password prompts

--- lib/common.sh
+++ lib/common.sh
@@ -106,11 +106,10 @@ prompt_password() {
         echo "" >&2
         echo "$prompt_text:" >&2
         read -s password </dev/tty
-        echo "" >&2
-        echo "" >&2
+        echo >&2
         echo "Confirm password:" >&2
         read -s password_confirm </dev/tty
-        echo "" >&2
+        echo >&2
 
         if [ "$password" = "$password_confirm" ]; then
             echo "$password"
